a5e7422e3aeec3e7efab6b149b930b53c692242b
Kush Pradhan <115583652+kushpradhan22@users.noreply.github.com>
2026-02-02 19:46:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily: {
+  instagram: ["Billabong", "cursive"],
+}
+};
+
